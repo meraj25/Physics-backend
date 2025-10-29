7a7ed0f91f903ea38655e60735196cdbb9be9886
Meraj <mbd.banu@gmail.com>
2025-10-29 15:17:28 +0530

    db connection integrated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import ContentRouter from './api/content.js';
+import connectDB from './infrastructure/db/index.js';
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use('/api', ContentRouter);
 
 const PORT = 3000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
