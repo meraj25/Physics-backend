6782c3b799748ad272ed0e1f08a685afd1b58a3c
Meraj <mbd.banu@gmail.com>
2025-10-29 15:40:04 +0530

    installed dotenv

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import ContentRouter from './api/content.js';
 import connectDB from './infrastructure/db/index.js';
--- infrastructure/db/index.js
+++ infrastructure/db/index.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const MONGODB_URL = process.env.MONGODB_URL;
-    if (!MONGODB_URL) {
+    const MONGODB_URI = process.env.MONGODB_URI;
+    if (!MONGODB_URI) {
       throw new Error("MongoDB connection string is not defined");
     }
-    await mongoose.connect(MONGODB_URL);
+    await mongoose.connect(MONGODB_URI);
     console.log("Connected to the Database");
   } catch (error) {
     if (error instanceof Error) {
