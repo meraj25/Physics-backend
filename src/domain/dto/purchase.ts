05e9e4d6cbd845616b38a5511276990d89d2bd3b
Meraj <mbd.banu@gmail.com>
2026-03-30 22:54:58 +0530

    ,

--- src/domain/dto/purchase.ts
+++ src/domain/dto/purchase.ts
@@ -5,10 +5,8 @@ const CreatePurchaseDTO = z.object({
   userId: z.string().min(1).optional(),
   username: z.string().min(1).optional(),
   contentId: z.string().min(1),
-  amount: z.number().min(1).optional(),
+  amount: z.number().min(0).optional(),  // also changed min(1) to min(0) in case price is 0
   paidAt: z.date().optional(),
-}).refine((d) => d.userId || d.username, {
-  message: "Either userId or username is required",
-});
+})
 
 export { CreatePurchaseDTO };
