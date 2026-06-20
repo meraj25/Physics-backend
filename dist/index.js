94e8614bd42aec5897928bcefa6670dbd8443dcf
Meraj <mbd.banu@gmail.com>
2026-06-20 12:52:38 +0530

    .

--- dist/index.js
+++ dist/index.js
@@ -32,7 +32,7 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)({
-    origin: "http://localhost:5173",
+    origin: "https://sanjayasuriya.online",
 }));
 app.use((0, express_2.clerkMiddleware)());
 app.use((req, res, next) => {
